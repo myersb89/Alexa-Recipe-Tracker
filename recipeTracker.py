274c39c15172b5ded792678701f519ee81cdb8bc
myersb89 <myersb89@gmail.com>
2018-09-24 10:18:15 -0700

    Updating with basic Alexa intents

--- recipeTracker.py
+++ recipeTracker.py
@@ -10,3 +10,80 @@ sb = SkillBuilder()
 
 def hello():
     return "hello world"
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        speech_text = "Welcome to the Recipe Tracker Skill. Say 'start a recipe' or 'create a recipe' to start tracking!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Launch Recipe Tracker", speech_text)).set_should_end_session(False)
+        return handler_input.response_builder.response
+
+class NewRecipeIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("NewRecipeIntent")(handler_input)
+
+    def handle(self, handler_input):
+        speech_text = "This functionality is not yet implemented. Please try again later"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Recipe Tracker", speech_text)).set_should_end_session(
+            True)
+        return handler_input.response_builder.response
+
+class HelpIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("AMAZON.HelpIntent")(handler_input)
+
+    def handle(self, handler_input):
+        speech_text = "Recipe Tracker lets you record ingredients as you cook so you can easily track the calories and macros. Say 'start a recipe' or 'create a recipe' to get started."
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Recipe Tracker", speech_text))
+        return handler_input.response_builder.response
+
+class CancelAndStopIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("AMAZON.CancelIntent")(handler_input) or is_intent_name("AMAZON.StopIntent")(handler_input)
+
+    def handle(self, handler_input):
+        speech_text = "Goodbye!"
+
+        handler_input.response_builder.speak(speech_text).set_card(
+            SimpleCard("Recipe Tracker", speech_text))
+        return handler_input.response_builder.response
+
+class SessionEndedRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_request_type("SessionEndedRequest")(handler_input)
+
+    def handle(self, handler_input):
+        #any cleanup logic goes here.
+        return handler_input.response_builder.response
+
+class AllExceptionHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        return True
+
+    def handle(self, handler_input, exception):
+        #log exception in CloudWatch logs
+        print(exception)
+
+        speech_text = "Sorry, I didn't get it. Can you please say it again?"
+        handler_input.response_builder.speak(speech_text).ask(speech_text)
+        return handler_input.response_builder.response
+
+
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_request_handler(HelpIntentHandler())
+sb.add_request_handler(CancelAndStopIntentHandler())
+sb.add_request_handler(SessionEndedRequestHandler())
+
+sb.add_request_handler(NewRecipeIntentHandler())
+
+sb.add_exception_handler(AllExceptionHandler())
+
+handler = sb.lambda_handler()
